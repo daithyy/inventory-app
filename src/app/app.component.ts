277793f5aaf7194d84143c11ce6001086c2c2d23
David Brennan <david.brennan@mail.itsligo.ie>
2018-02-28 12:29:10 +0000

    Add Product model.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <div class="inventory-app">
+    
+  </div>
+  `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  product : Product;
+
+  constructor() {
+    this.product = new Product(
+      'NICEHAT',
+      'A Nice Black Hat',
+      '/resources/images/products/black-hat.jpg',
+      ['Men', 'Accesories', 'Hats'], 29.99);
+  }
 }
